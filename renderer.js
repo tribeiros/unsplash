af504cc5de24a3c8178f7fa79ded59858b040d03
tribeiros <tribeiros@indracompany.com>
2018-05-05 19:16:57 -0300

    removed jsfade

--- renderer.js
+++ renderer.js
@@ -10,25 +10,4 @@ for (let item of cubes) {
   	item.style.opacity = 1;
 }
 
-function fadeIn(el, time) {
-  el.style.opacity = 0;
-
-  var last = +new Date();
-  var tick = function() {
-    el.style.opacity = +el.style.opacity + (new Date() - last) / time;
-    last = +new Date();
-
-    if (+el.style.opacity < 1) {
-      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16);
-    }
-  };
-
-  tick();
-}
-
-var el = document.getElementById("grid");
-fadeIn(el, 3000);
-
-
-
 setInterval(function(){ location.reload(); }, 60000);
