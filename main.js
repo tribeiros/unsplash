5a09adf6291291151aa2868fc8e2186a0c7fc5d2
tribeiros <tribeiros@indracompany.com>
2018-05-05 15:24:37 -0300

    1280x720

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const shell = require('electron')
 
 let mainWindow
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1280, height: 720})
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
