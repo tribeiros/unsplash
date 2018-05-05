26a34fac9166e4f1c7c417a72e9a4082043aee93
tribeiros <tribeiros@indracompany.com>
2018-05-05 18:57:10 -0300

    nature

--- renderer.js
+++ renderer.js
@@ -1,10 +1,11 @@
 const unsplashApi = 'https://source.unsplash.com'
-const source = 'random'
+const source = ''
 const resolution = '1920x1080'
+const search = '?nature'
 const cubes = document.getElementsByClassName('cube');
 
 for (let item of cubes) {
-  const bgImage = `url('${unsplashApi}/${source}/${resolution}')`;
+  const bgImage = `url('${unsplashApi}/${source}/${resolution}/${search}')`;
   item.style.backgroundImage = `linear-gradient(
     rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
   item.style.opacity = 1;
