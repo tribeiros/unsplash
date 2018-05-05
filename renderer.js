654c428f88c8bcf50b93281e3c56a6f2fa867224
tribeiros <tribeiros@indracompany.com>
2018-05-05 17:40:17 -0300

    learn

--- renderer.js
+++ renderer.js
@@ -1,18 +1,21 @@
-const getRandomNum = () => {
+/*const getRandomNum = () => {
   return Math.floor(Math.random() * 1);
-}
+}*/
 
 const unsplashApi = 'https://source.unsplash.com'
 const source = 'random'
 const resolution = '1920x1080'
 const cubes = document.getElementsByClassName('cube');
 
+
+function euvouconseguir(){
 for (let item of cubes) {
-  const bgImage = `url('${unsplashApi}/${source}/${resolution}${getRandomNum()}')`;
+  const bgImage = `url('${unsplashApi}/${source}/${resolution}')`;
   item.style.backgroundImage = `linear-gradient(
     rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
   item.style.opacity = 1;
 }
-
+};
 
 setInterval(function(){ location.reload(); }, 60000);
+euvouconseguir()
