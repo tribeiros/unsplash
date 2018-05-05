e7734b89cd00c48bb4bec4b3676f01bdf4bbd26f
tribeiros <tribeiros@indracompany.com>
2018-05-05 17:16:56 -0300

    update

--- renderer.js
+++ renderer.js
@@ -2,10 +2,13 @@ const getRandomNum = () => {
   return Math.floor(Math.random() * 1);
 }
 
+const unsplashApi = 'https://source.unsplash.com'
+const source = 'random'
+const resolution = '1920x1080'
 const cubes = document.getElementsByClassName('cube');
 
 for (let item of cubes) {
-  const bgImage = `url('https://source.unsplash.com/random/1920x1080${getRandomNum()}')`;
+  const bgImage = `url('${unsplashApi}/${source}/${resolution}${getRandomNum()}')`;
   item.style.backgroundImage = `linear-gradient(
     rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
   item.style.opacity = 1;
