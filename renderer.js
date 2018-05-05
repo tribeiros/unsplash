799909b3b27fdb7f58c16f38667ad084cc384980
tribeiros <tribeiros@indracompany.com>
2018-05-05 15:42:32 -0300

    interval()

--- renderer.js
+++ renderer.js
@@ -4,12 +4,14 @@ const getRandomNum = () => {
 
 const cubes = document.getElementsByClassName('cube');
 
+setInterval(function(){
+
 for (let item of cubes) {
   const bgImage = `url('https://source.unsplash.com/random/1920x1080${getRandomNum()}')`;
   item.style.backgroundImage = `linear-gradient(
     rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
   item.style.opacity = 1;
-}
+}; }, 15000);
 
 
-setInterval(function(){ location.reload(); }, 15000);
+//setInterval(function(){ location.reload(); }, 15000);
