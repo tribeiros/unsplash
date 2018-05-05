4b8691f8a6a6f10658f1be80cb2c2dd6893279e5
tribeiros <tribeiros@indracompany.com>
2018-05-05 16:41:31 -0300

    added linear

--- renderer.js
+++ renderer.js
@@ -6,7 +6,8 @@ const cubes = document.getElementsByClassName('cube');
 
 for (let item of cubes) {
   const bgImage = `url('https://source.unsplash.com/random/1920x1080${getRandomNum()}')`;
-  item.style.backgroundImage = `${bgImage}`;
+  item.style.backgroundImage = `linear-gradient(
+    rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
   item.style.opacity = 1;
 }
 
