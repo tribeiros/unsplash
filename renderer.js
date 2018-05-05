551c59c94c432292ba498dbbb9fdb46c4ec652d7
tribeiros <tribeiros@indracompany.com>
2018-05-05 16:37:53 -0300

    removed linear

--- renderer.js
+++ renderer.js
@@ -6,8 +6,7 @@ const cubes = document.getElementsByClassName('cube');
 
 for (let item of cubes) {
   const bgImage = `url('https://source.unsplash.com/random/1920x1080${getRandomNum()}')`;
-  item.style.backgroundImage = `linear-gradient(
-    rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), ${bgImage}`;
+  item.style.backgroundImage = `${bgImage}`;
   item.style.opacity = 1;
 }
 
